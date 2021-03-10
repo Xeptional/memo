e90728279b4ea2111554a21eb36019a12a13db7a
xeptional <xeptionalprof@gmail.com>
2021-03-10 14:17:49 +0100

    just some little changes

--- newproject/src/action/types.js
+++ newproject/src/action/types.js
@@ -10,3 +10,6 @@ export const GET_ERRORS = "GET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const SHOW_LOADER = "SHOW_LOADER";
 export const HIDE_LOADER = "HIDE_LOADER";
+export const MEMO_SUCCESS = "MEMO_SUCCESS";
+export const MEMO_FAILURE = "MEMO_FAILURE";
+export const MEMO_PENDING = "MEMO_PENDING";
--- newproject/src/component/Tab/Tabs/Form.js
+++ newproject/src/component/Tab/Tabs/Form.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
 import { TextField, Button } from "@material-ui/core";
+import UploadButton from "../../Uploadbtn";
 
 class Form extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       memoFrom: "",
+       memoTo: "",
+       memoTitle: "",
+       memoRemark: "",
+    }
+  }
+  
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+
+  }
+
   render() {
     return (
       <div style={{ paddingTop: "50px" }}>
@@ -10,6 +29,7 @@ class Form extends Component {
           <TextField
             style={{ width: "80%" }}
             id="outlined-basic"
+            name="memoFrom"
             variant="outlined"
           />
         </div>
@@ -18,6 +38,7 @@ class Form extends Component {
           <TextField
             style={{ width: "80%" }}
             id="outlined-basic"
+            name="memoTO"
             variant="outlined"
           />
         </div>
@@ -26,6 +47,7 @@ class Form extends Component {
           <TextField
             style={{ width: "80%" }}
             id="outlined-basic"
+            name="memoTitle"
             variant="outlined"
           />
         </div>
@@ -34,8 +56,15 @@ class Form extends Component {
           <TextField
             style={{ width: "80%" }}
             id="outlined-basic"
+            name="memoRemark"
             variant="outlined"
           />
+        </div>
+        <div style={{ paddingTop: "20px" }}>
+          <UploadButton />
+        </div>
+        <div style={{ paddingTop: "20px" }}>
+          <Button variant="contained" onClick={e => this.handleSubmit}>Submit</Button>
         </div>
       </div>
     );
